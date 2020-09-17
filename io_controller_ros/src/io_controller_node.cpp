42dce9d703bd44f099f24f6225bf17412c69b3c4
Stevie Dale <sdale4@asu.edu>
2020-09-17 14:51:52 -0500

    added custom service for setting I/O port

--- io_controller_ros/src/io_controller_node.cpp
+++ io_controller_ros/src/io_controller_node.cpp
@@ -1,7 +1,15 @@
 #include "io_controller/io_controller.h"
 
-int main(int argc, char **argv)
+#include <rclcpp/rclcpp.hpp>
+#include <std_srvs/SetBool.h>
+
+#include <memory>
+
+void cb(const std::shared_ptr<std_srvs::SetBool::Request> request,
+         std::shared_ptr<std_srvs::SetBool::Response> response)
 {
+  RCLCPP_WARN(rclcpp::get_logger("rclcpp"), "Request received");
+  /*
   IOController io_controller;
   io_controller.setPort(0, true);
   io_controller.setPort(0, false);
@@ -11,5 +19,22 @@ int main(int argc, char **argv)
   io_controller.setPort(2, false);
   io_controller.setPort(3, true);
   io_controller.setPort(3, false);
+  */
+}
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("io_controller_node");
+
+  rclcpp::Service<std_srvs::SetBool>::SharedPtr service =
+    node->create_service<std_srvs::SetBool>("io_controller_service", &cb);
+
+  RCLCPP_WARN(rclcpp::get_logger("rclcpp"), "Spinin'");
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
   return 0;
 }
